32f5a4b154be2fffaf0e5c7721f56c0cf6ecf081
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 12:34:06 +0100

    Create client Menu component unit tests

---
+++ client/src/components/__tests__/Menu.test.tsx
@@ -0,0 +1,52 @@
+import { render } from "@testing-library/react";
+
+import Menu from "../Menu";
+
+const empty = (): true => {
+    return true;
+}
+
+const props = {
+    logOut: empty,
+    uploadModal: empty,
+    deleteModal: empty,
+    selectAllPhotos: empty,
+    deselectPhotos: empty,
+    reverse: false,
+    reverseEvent: empty,
+    sortEvent: empty,
+    sortBy: "Date",
+    navbarDisplay: "block"
+}
+
+test("Menu is displaying selected items properly and expanding size based on it", () => {
+
+    const { getByTestId, getByText, rerender } = render(<Menu {...props} selectionCount={0} />)
+
+    expect(getByTestId("menu")).toHaveClass("navbar-expand-lg");
+
+    expect(getByText(/Selected 0 items/)).toBeInTheDocument();
+
+    rerender(<Menu {...props} selectionCount={2} />)
+
+    expect(getByTestId("menu")).toHaveClass("navbar-expand-xl");
+
+    expect(getByText(/Selected 2 items/)).toBeInTheDocument();
+
+})
+
+test("Menu is displaying all buttons and logo properly", () => {
+
+    const { getByText} = render(<Menu {...props} selectionCount={1} />)
+
+    expect(getByText(/Upload new image/)).toBeInTheDocument();
+
+    expect(getByText(/Delete/)).toBeInTheDocument();
+
+    expect(getByText(/Cancel selection/)).toBeInTheDocument();
+
+    expect(getByText(/Select all/)).toBeInTheDocument();
+
+    expect(getByText(/Logout/)).toBeInTheDocument();
+
+})
