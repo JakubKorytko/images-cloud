95425e00ee484e8a1e9e7e1593e19fae18d707b7
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:52:54 +0100

    Prepare server ImageModel

---
+++ server/src/models/image.model.ts
@@ -0,0 +1,12 @@
+export interface ImageModel {
+    imageId: number,
+    name: string,
+    path: string,
+    thumb_path: string,
+    progressive_path: string,
+    size: number,
+    width: number,
+    height: number,
+    date: number,
+    ratioY: number
+}
