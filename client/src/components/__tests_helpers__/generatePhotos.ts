9f0c5e9f2902915bba0ecc41e2a453d714dba2c4
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 12:34:19 +0100

    Add client Gallery component unit tests

---
+++ client/src/components/__tests_helpers__/generatePhotos.ts
@@ -0,0 +1,35 @@
+import { Photo } from "../../types/photoObject"
+
+const pseudoRandom = (i: number, j: number): number => {
+    if (i%2) return Math.round(1/++i * j)
+    else return Math.round(++i * j)
+}
+
+const testImages = (amount: number): Photo[] => {
+    let images: Photo[] = [];
+
+    for (let i=0; i<amount; i++) {
+    
+        const width=pseudoRandom(i, 400);
+        const height=pseudoRandom(i, 200);
+    
+        const imageSize = `${width}x${height}?text=${width}%20x%20${height}`;
+    
+        const photo = { 
+            imageId: i,
+            name: `image${i}.png`,
+            path: `https://via.placeholder.com/${imageSize}`,
+            size: pseudoRandom(i, 10**3),
+            width: width,
+            height: height,
+            date: pseudoRandom(i, 10**6),
+            ratioY: height/width
+        };
+    
+        images.push(photo);
+    }
+
+    return images;
+}
+
+module.exports = testImages;
