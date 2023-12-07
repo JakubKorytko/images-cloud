c658beab483dfc989e2e77ac388d2d535c27de8f
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 11:56:32 +0100

    Add server authorization middleware

---
+++ server/src/middlewares/authorization.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+const {decode} = require("../utils/auth.util");
+require('dotenv').config();
+const secret = process.env.APP_SECRET;
+
+const authorization = function (req: Request, res: Response, next: NextFunction) {
+
+    const token:string | undefined = req.headers.authorization;
+
+    const authData = decode(secret)(token);
+    // console.log(authData);
+    // console.log(req.url);
+    if (authData == false) {
+      res.status(401).send("Not authorized or wrong token given");
+      return false;
+    }
+    
+    res.locals.auth = authData;
+    next();
+}
+
+module.exports = authorization;
