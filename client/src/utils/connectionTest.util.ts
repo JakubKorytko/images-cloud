4217bc6b6ed09fee197672923069661524b43874
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 12:03:19 +0100

    Create client connection test utility

---
+++ client/src/utils/connectionTest.util.ts
@@ -0,0 +1,88 @@
+import Token from "./token.util";
+const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+//  =================================================== 
+//                    RESPONSE CODES                    
+//  =================================================== 
+//   UNAUTH - Unauthorized or token expired             
+//   AUTH_ERROR - Error while trying to authenticate    
+//   SERVER_DOWN - Server is down                       
+//   AUTH_OK - Auth test passed along with server test  
+//   SERVER_OK - Server test passed                     
+//  =================================================== 
+
+
+// //=================================================================\\
+// ||                            authTest                             ||
+// |]=================================================================[|
+// || Function tests if server is running and returns object          ||
+// || with structure:                                                 ||
+// ||                                                                 ||
+// || {                                                               ||
+// || resCode - short code explaining res value                       ||
+// || msg - text representation of returned code                      ||
+// || res - boolean determining if test went ok                       ||
+// || redirect - url to redirect when needed                          ||
+// || data - object containing username - only if test was successful ||
+// || }                                                               ||
+// ||                                                                 ||
+// \\=================================================================//
+
+
+
+export function authTest(): Promise<{resCode: string, msg: string, res: boolean, data?: {username: string}, redirect: string}> {
+
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Authorization', `Bearer ${Token.value}`);
+
+    return new Promise((resolve, reject): void => {
+        serverTest().then(data => {
+            if (data.resCode === "SERVER_DOWN") resolve(data);
+            fetch(`${serverUrl}/auth`, {
+                method: "POST",
+                headers: requestHeaders
+            }).then(response => response.json()).then((json: {res: false|string} ): void => {
+                if (!json || !json.res) {
+                    Token.remove();
+                    resolve({resCode: "UNAUTH", msg: "Unauthorized or token expired", res: false, redirect: "/login?redirected" });
+                } else {
+                    resolve({resCode: "AUTH_OK", msg: "Authorized and server is running", res: true, data: {username: json.res}, redirect: "/" })
+                }
+            })
+        }).catch(error => {
+                resolve({resCode: "AUTH_ERROR", msg: "Error while trying to authenticate", res: false, redirect: "/status?redirected"});
+        })
+    })
+
+}
+
+
+// //========================================================\\
+// ||                       serverTest                       ||
+// |]========================================================[|
+// || Function tests if server is running and returns object ||
+// || with structure:                                        ||
+// ||                                                        ||
+// || {                                                      ||
+// || resCode - short code explaining res value              ||
+// || msg - text representation of returned code             ||
+// || res - boolean determining if test went ok              ||
+// || redirect - url to redirect when needed                 ||
+// || }                                                      ||
+// ||                                                        ||
+// \\========================================================//
+
+
+export function serverTest(): Promise<{resCode: string, msg: string, res: boolean, redirect: string}> {
+    return new Promise((resolve, reject): void => {
+        fetch(`${serverUrl}/health`).then(response => {
+            if (!response.ok) {
+                resolve({resCode: "SERVER_DOWN", msg: "Server is down", res: false, redirect: "/status?redirected"});
+            } else {
+                resolve({resCode: "SERVER_OK", msg: "Server is running", res: true, redirect: "/"});
+            }
+        }).catch(error => {
+            resolve({resCode: "SERVER_DOWN", msg: "Server is down", res: false, redirect: "/status?redirected"});
+        })
+    })
+}
