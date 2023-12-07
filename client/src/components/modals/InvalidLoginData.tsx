d050f6dc4c9fe948f16d3ff720ed252379ceec04
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 12:08:51 +0100

    Create client InvalidLoginData modal component

---
+++ client/src/components/modals/InvalidLoginData.tsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+class InvalidLoginData extends Component<{show: boolean, closeHandler: Function}, {}> {
+
+    closeHandler = (): void => {
+        this.props.closeHandler();
+    }
+
+    render() {
+        return (
+            <Modal centered show={this.props.show} onHide={this.closeHandler}>
+            <Modal.Header closeButton>
+              <Modal.Title className="text-danger">Wrong username or password!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Please try again</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.closeHandler}>
+                Okay...
+              </Button>
+            </Modal.Footer>
+          </Modal>
+  
+        );
+    }
+}
+
+export default InvalidLoginData;
