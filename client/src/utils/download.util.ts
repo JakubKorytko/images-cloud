a80c4df995f84e4a33f4f57e1b46e54198e425fb
JakubKorytko <jakub.korytko@o2.pl>
2023-12-07 12:03:32 +0100

    Create client download file utility

---
+++ client/src/utils/download.util.ts
@@ -0,0 +1,19 @@
+// //========================================\\
+// ||           DOWNLOAD FILE UTIL           ||
+// |]========================================[|
+// || Creates temporary "a" element,         ||
+// || sets link and name, appends it to DOM, ||
+// || clicks it and then removes it from DOM ||
+// \\========================================//
+
+export async function downloadFile(link: string, name: string): Promise<boolean> {
+    if (!link || !name) return false
+    let tmpLink: HTMLAnchorElement|null = document.createElement('a');
+    tmpLink.href = link
+    tmpLink.download = name;
+    document.body.appendChild(tmpLink);
+    tmpLink.click();
+    document.body.removeChild(tmpLink);
+    tmpLink = null;
+    return true;
+}
