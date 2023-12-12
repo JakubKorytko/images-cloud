ee20f808c4648f8de4e4d4eb6f0382016badbd18
Jakub Korytko <jakub@korytko.me>
2023-12-12 09:30:01 +0100

    Make ProgressiveImage a functional component

--- client/src/components/images/ProgressiveImage.tsx
+++ client/src/components/images/ProgressiveImage.tsx
@@ -1,91 +1,75 @@
-import { Component } from "react";
-import { ProgressiveImageProps, ProgressiveImageState } from "../../types/progressiveImage";
-import { fetchBlob } from "../../utils/fetchBlob.util";
+import {useEffect, useState} from "react";
+import {ProgressiveImageProps} from "../../types/progressiveImage";
+import {fetchBlob} from "../../utils/fetchBlob.util";
 import Token from "../../utils/token.util";
 
-class ProgressiveImage extends Component<ProgressiveImageProps, ProgressiveImageState> {
+const ProgressiveImage = (props: ProgressiveImageProps) => {
 
-  private _isMounted: boolean
+    const [imgPlaceholder, setImgPlaceholder] = useState("");
+    const [imgSrc, setImgSrc] = useState({
+        url: `https://via.placeholder.com/${props.imageSize}`,
+        loading: true
+    });
 
-  constructor(props: ProgressiveImageProps) {
-    super(props);
-    this.state = {
-      loading: true,
-      currentSrc: `https://via.placeholder.com/${this.props.imageSize}`,
-      currentName: "",
-      preBlob: "",
-      loadingImage: "",
-      fetching: false,
-    };
-    this._isMounted = false;
-  }
+    const refreshPhoto = (): void => {
 
-  refreshPhoto = (): void => {
-    const requestHeaders: HeadersInit = new Headers();
-    requestHeaders.set('Authorization', `Bearer ${Token.value}`);
+        const requestHeaders: HeadersInit = new Headers();
+        requestHeaders.set('Authorization', `Bearer ${Token.value}`);
 
-    const { src } = this.props;
-    const imageToLoad = new Image();
-    fetch(src, { headers: requestHeaders })
-      .then(response => {
-        if (response.ok) {
-          return response.blob();
-        }
-        window.location.href="/status?redirected";
-        throw new Error('Something went wrong');
-      })
-      .then(imageBlob => {
-        const imageObjectURL = URL.createObjectURL(imageBlob);
-        imageToLoad.src = imageObjectURL;
-        if (this._isMounted) this.setState({ loadingImage: imageObjectURL })
-        imageToLoad.onload = () =>
-          {if (this._isMounted) this.setState({ currentSrc: this.state.loadingImage, loading: false, fetching: false })};
-      })
-      .catch(error => {
-        window.location.href="/status?redirected";
-        throw new Error(error);
-      });
-
-  }
-
-  async componentDidMount() {
-    this._isMounted = true;
-    await this.setImage();
-  }
-
-  async componentDidUpdate() {
-    await this.setImage();
-  }
-
-  setImage = async (): Promise<void> => {
-    if (this.state.currentName !== this.props.placeholder) {
-      if (this._isMounted) await this.setState({ currentName: this.props.placeholder });
-      if (this._isMounted) await this.setState({fetching: true});
-      const url = await fetchBlob(this.props.placeholder);
-      if (this._isMounted) await this.setState({ currentSrc: url })
-      this.refreshPhoto();
+        const { src } = props;
+        const imageToLoad = new Image();
+        fetch(src, { headers: requestHeaders })
+          .then(response => {
+            if (response.ok) {
+              return response.blob();
+            }
+            window.location.href="/status?redirected";
+            throw new Error('Something went wrong');
+          })
+          .then(imageBlob => {
+              imageToLoad.src = URL.createObjectURL(imageBlob);
+            imageToLoad.onload = (x) => {
+                const img = x.target as HTMLImageElement;
+                setImgSrc({url: img.src, loading: false});
+            }
+          })
+          .catch(error => {
+            window.location.href="/status?redirected";
+            throw new Error(error);
+          });
     }
-  }
 
-  componentWillUnmount() {this._isMounted = false;}
+    useEffect(() => {
+        if (imgPlaceholder !== props.placeholder) {
+            setImgPlaceholder(props.placeholder);
+            fetchBlob(props.placeholder).then(
+                blob => {
+                    setImgSrc({...imgSrc, url: blob});
+                    refreshPhoto();
+                }
 
-  render() {
-    const { loading } = this.state;
+            );
+        }
+    });
 
-    const opacity = loading ? 50 : 100
-    const checked = (this.props.checkState) ? "-checked" : ""
+    const imgClassNames = [
+        `opacity-${imgSrc.loading ? 50 : 100}`,
+        `progressive-image${props.checkState ? "-checked" : ""}`,
+        "thumbnail",
+        "w-100"
+    ]
+
+    const imgAttributes = {
+        src: imgSrc.url,
+        className: imgClassNames.join(" "),
+        onClick: props.click,
+        "data-testid": "proggresive_img",
+        "image-id": props.imageId,
+    }
 
     return (
-      <img
-        data-testid="proggresive_img" 
-        src={this.state.currentSrc}
-        className={`opacity-${opacity} progressive-image${checked} thumbnail w-100`}
-        image-id={this.props.imageId}
-        alt={this.props.name}
-        onClick={this.props.click}
-      />
-    );
-  }
+        <img {...imgAttributes} alt={props.name} />
+    )
 }
 
 export default ProgressiveImage;
